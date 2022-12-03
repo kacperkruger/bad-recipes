df627ea07f9ad1e2600d4111b6b4d5d2e7a88692
Kacper Kruger <kacper.kruger1@gmail.com>
2022-12-03 18:23:45 +0100

    added service

---
+++ NotificationSMS/src/main/java/com/github/kacperkruger/notificationsms/service/NotificationSMSService.java
@@ -0,0 +1,35 @@
+package com.github.kacperkruger.notificationsms.service;
+
+import com.github.kacperkruger.notificationsms.domain.SMSRequest;
+import com.github.kacperkruger.notificationsms.sender.SMSSender;
+import com.github.kacperkruger.notificationsms.service.error.InvalidMessage;
+import com.github.kacperkruger.notificationsms.service.error.InvalidPhoneNumber;
+import org.springframework.stereotype.Service;
+
+import java.util.regex.Pattern;
+
+@Service
+public class NotificationSMSService {
+
+    private final Pattern PHONE_PATTERN = Pattern.compile("^[+][0-9]{11}");
+
+    private final SMSSender smsSender;
+
+    public NotificationSMSService(SMSSender smsSender) {
+        this.smsSender = smsSender;
+    }
+
+    public void validateAndSend(SMSRequest smsRequest) throws InvalidPhoneNumber, InvalidMessage {
+        if (!validatePhoneNumber(smsRequest.getPhoneNumber())) throw new InvalidPhoneNumber();
+        if (!validateMessage(smsRequest.getMessage())) throw new InvalidMessage();
+        smsSender.send(smsRequest);
+    }
+
+    public boolean validatePhoneNumber(String phoneNumber) {
+        return PHONE_PATTERN.matcher(phoneNumber).matches();
+    }
+
+    public boolean validateMessage(String message) {
+        return !message.isEmpty();
+    }
+}
