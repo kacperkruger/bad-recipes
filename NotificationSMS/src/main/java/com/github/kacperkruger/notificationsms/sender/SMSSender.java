d56709523148559710d08b8a9e29f4a53041add4
Kacper Kruger <kacper.kruger1@gmail.com>
2022-12-03 19:51:34 +0100

    changed directory and module name to lower camel case

--- NotificationSMS/src/main/java/com/github/kacperkruger/notificationsms/sender/SMSSender.java
+++
@@ -1,8 +0,0 @@
-package com.github.kacperkruger.notificationsms.sender;
-
-import com.github.kacperkruger.notificationsms.domain.SMSRequest;
-
-public interface SMSSender {
-
-    void send(SMSRequest smsRequest);
-}
