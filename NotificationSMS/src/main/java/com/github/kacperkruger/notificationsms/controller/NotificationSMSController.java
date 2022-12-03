ef5454437fa834ab664d481572ea140e23acd0a7
Kacper Kruger <kacper.kruger1@gmail.com>
2022-12-03 18:23:57 +0100

    added api controller

---
+++ NotificationSMS/src/main/java/com/github/kacperkruger/notificationsms/controller/NotificationSMSController.java
@@ -0,0 +1,28 @@
+package com.github.kacperkruger.notificationsms.controller;
+
+import com.github.kacperkruger.notificationsms.domain.SMSRequest;
+import com.github.kacperkruger.notificationsms.service.NotificationSMSService;
+import com.github.kacperkruger.notificationsms.service.error.InvalidMessage;
+import com.github.kacperkruger.notificationsms.service.error.InvalidPhoneNumber;
+import jakarta.validation.Valid;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/notification-sms")
+public class NotificationSMSController {
+
+    private final NotificationSMSService smsService;
+
+    public NotificationSMSController(NotificationSMSService smsService) {
+        this.smsService = smsService;
+    }
+
+    @PostMapping
+    public void sendSMS(@Valid @RequestBody SMSRequest smsRequest) throws InvalidPhoneNumber, InvalidMessage {
+        smsService.validateAndSend(smsRequest);
+    }
+}
