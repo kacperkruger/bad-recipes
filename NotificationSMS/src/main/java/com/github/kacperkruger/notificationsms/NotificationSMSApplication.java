2c8f6f0d1587da9beb507e2f468618f74ca042f6
Kacper Kruger <kacper.kruger1@gmail.com>
2022-12-03 18:23:08 +0100

    init spring application

---
+++ NotificationSMS/src/main/java/com/github/kacperkruger/notificationsms/NotificationSMSApplication.java
@@ -0,0 +1,12 @@
+package com.github.kacperkruger.notificationsms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationSMSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationSMSApplication.class, args);
+    }
+}
