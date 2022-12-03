a5680f5245d63f467936cfc1587b07a624b991e9
Kacper Kruger <kacper.kruger1@gmail.com>
2022-12-03 18:20:59 +0100

    added twilio config

---
+++ NotificationSMS/src/main/java/com/github/kacperkruger/notificationsms/config/TwilioConfig.java
@@ -0,0 +1,32 @@
+package com.github.kacperkruger.notificationsms.config;
+
+import com.twilio.Twilio;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TwilioConfig {
+
+    @Value("${twilio.account_sid}")
+    private String accountSid;
+
+    @Value("${twilio.auth_token}")
+    private String authToken;
+
+    @Value("${twilio.phone_number}")
+    private String twilioPhoneNumber;
+
+    @Bean
+    public void twilioInitializer() {
+        Twilio.init(
+                accountSid,
+                authToken
+        );
+    }
+
+    @Bean
+    public String twilioPhoneNumber() {
+        return twilioPhoneNumber;
+    }
+}
