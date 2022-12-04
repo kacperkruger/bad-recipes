767271355eaa5bd1e38ea6ca31bc1212e3085733
Kacper Kruger <kacper.kruger1@gmail.com>
2022-12-04 15:18:02 +0100

    added annotation

--- notification/src/main/java/com/github/kacperkruger/notification/repository/NotificationRepository.java
+++ notification/src/main/java/com/github/kacperkruger/notification/repository/NotificationRepository.java
@@ -2,6 +2,8 @@ package com.github.kacperkruger.notification.repository;
 
 import com.github.kacperkruger.notification.domain.Notification;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface NotificationRepository extends CrudRepository<Long, Notification> {
 }
