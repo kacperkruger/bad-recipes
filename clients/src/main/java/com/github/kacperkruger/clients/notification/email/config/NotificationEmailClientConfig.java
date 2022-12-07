91ce647f953be9a5975f67c382085dd60ad457da
Kacper Kruger <kacper.kruger1@gmail.com>
2022-12-07 22:14:54 +0100

    added config for web client

--- clients/src/main/java/com/github/kacperkruger/clients/notification/email/config/NotificationEmailClientConfig.java
+++ clients/src/main/java/com/github/kacperkruger/clients/notification/email/config/NotificationEmailClientConfig.java
@@ -1,12 +1,16 @@
 package com.github.kacperkruger.clients.notification.email.config;
 
-import feign.codec.ErrorDecoder;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
+@Configuration
 public class NotificationEmailClientConfig {
 
     @Bean
-    public ErrorDecoder emailRequestErrorDecoder() {
-        return new NotificationEmailErrorDecoder();
+    public WebClient getWebClient() {
+        return WebClient.create();
     }
 }
