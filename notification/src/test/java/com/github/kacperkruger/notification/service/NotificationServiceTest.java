595ebc2f27ad65d57117157024f68d6d3ded54b5
Kacper Kruger <kacper.kruger1@gmail.com>
2022-12-14 13:34:22 +0100

    added test that always fail

---
+++ notification/src/test/java/com/github/kacperkruger/notification/service/NotificationServiceTest.java
@@ -0,0 +1,33 @@
+package com.github.kacperkruger.notification.service;
+
+import com.github.kacperkruger.clients.notification.service.NotificationSenderStrategyResolver;
+import com.github.kacperkruger.notification.repository.NotificationRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+
+public class NotificationServiceTest {
+
+    @MockBean
+    NotificationRepository notificationRepository;
+
+    NotificationService notificationService;
+
+    @MockBean
+    NotificationSenderStrategyResolver notificationSenderResolver;
+
+    @BeforeEach
+    void setUp() {
+        NotificationRepository mockRepository = mock(NotificationRepository.class);
+        NotificationSenderStrategyResolver mockSenderResolver = mock(NotificationSenderStrategyResolver.class);
+        notificationService = new NotificationService(mockSenderResolver, mockRepository);
+    }
+
+    @Test
+    void test() {
+        assertTrue(false);
+    }
+}
